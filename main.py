c81fa6fcff85191f44161b2feef4683d8183ce7d
Waqas Ahmed <waqas.ghaloo@gmail.com>
2023-08-26 13:16:36 +0300

    added function to upload data from local folder to cloud storage

--- main.py
+++ main.py
@@ -1,18 +1,32 @@
 from google.cloud import storage
+import os
 
-# this code is from code cloud IDE extension, first follow instruction in API to enable, authenticate, login your
-# IDE with your email which is associated with account on GCP
-
-def create_bucket(bucket_name, project_id):
-    """Creates a new bucket."""
+def create_bucket_if_not_exists(bucket_name, project_id):
+    """Creates a new bucket if it doesn't already exist."""
 
     storage_client = storage.Client(project=project_id)
+    bucket = storage_client.bucket(bucket_name)
 
-    bucket = storage_client.create_bucket(bucket_name)
+    if not bucket.exists():
+        bucket.create()
+        print(f"Bucket {bucket.name} created")
+    else:
+        print(f"Bucket {bucket.name} already exists")
 
-    print(f"Bucket {bucket.name} created")
+def upload_files_to_bucket(bucket_name, project_id, source_folder):
+    storage_client = storage.Client(project=project_id)
+    bucket = storage_client.get_bucket(bucket_name)
+
+    for root, dirs, files in os.walk(source_folder):
+        for file in files:
+            local_file_path = os.path.join(root, file)
+            blob = bucket.blob(file)
+            blob.upload_from_filename(local_file_path)
+            print(f"Uploaded {file} to {bucket_name}")
 
 bucket_name = "waqasghaloo1010"
 project_id = "copper-harbor-397011"  # Replace with your actual project ID
+source_folder = "E:\Personal Laptop Data\Docs\Certifications\GETCERT_DE\MODULES"  # Replace with the path to your local folder
 
-create_bucket(bucket_name, project_id)
+create_bucket_if_not_exists(bucket_name, project_id)
+upload_files_to_bucket(bucket_name, project_id, source_folder)
